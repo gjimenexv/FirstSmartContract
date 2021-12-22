4ce61cf72daef34ea724369e86f9d8d87c00327e
Guillermo Jimenez <guillejim5@gmail.com>
2021-12-21 23:11:23 -0600

    Class 17 Enums. (other class changes are included)

--- FirstSmartContract.sol
+++ FirstSmartContract.sol
@@ -3,13 +3,26 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract CrowdFunding {
-    string public id;
-    string public name;
-    string public description;
-    address payable public author;
-    string public state = "Opened";
-    uint256 public funds;
-    uint256 public fundraisingGoal;
+    //This is an enum to define valid values for a variables
+     enum State {Active,Inactive}
+
+    //This is a struct to create kind of a object.
+    struct Project {
+        string id;
+        string name;
+        string description;
+        address payable author;
+        State state;
+        uint256 funds;
+        uint256 fundraisingGoal;
+    }
+    //This is a struct declaration
+    Project public project;
+
+    //This is a event use to provide logs un execution runtime
+    event ProjectFunded(string projectId, uint256 value);
+    //This is a event use to provide logs un execution runtime
+    event ProjectStateChanged(string id, State state);
 
     constructor(
         string memory _id,
@@ -17,27 +30,49 @@ contract CrowdFunding {
         string memory _description,
         uint256 _fundraisingGoal
     ) {
-        id = _id;
-        name = _name;
-        description = _description;
-        fundraisingGoal = _fundraisingGoal;
-        author = payable(msg.sender);
+       //This is a struct build. attribute order matters.
+        project = Project(
+            _id,
+            _name,
+            _description,
+            payable(msg.sender),
+            State.Active,
+            0,
+            _fundraisingGoal
+        );
     }
-    modifier onlyOwner(){
-        require(msg.sender == author,"Just the owner can do this action");
-         _;
+    //This is modifier to define in a function who can invoke it
+    modifier isAuthor() {
+        require(
+            project.author == msg.sender,
+            "You need to be the project author"
+        );
+        _;
     }
-    modifier excludeOwner(){
-        require(msg.sender != author,"Owner can not do this action");
-         _;
+    //This is modifier to define in a function who can invoke it
+    modifier isNotAuthor() {
+        require(
+            project.author != msg.sender,
+            "As author you can not fund your own project"
+        );
+        _;
     }
 
-    function fundProject() public payable excludeOwner {
-        author.transfer(msg.value);
-        funds += msg.value;
+    function fundProject() public payable isNotAuthor {
+        //Validation using requiere to avoid funding when state is Inactive
+        require(project.state != State.Inactive, "The project can not receive funds");
+        //Validation using requiere to avoid funding of zero.
+        require(msg.value > 0, "Fund value must be greater than 0");
+        project.author.transfer(msg.value);
+        project.funds += msg.value;
+        //Invoke the event kind to provide the relevant info
+        emit ProjectFunded(project.id, msg.value);
     }
 
-    function changeProjectState(string calldata newState) public onlyOwner {
-        state = newState;
+    function changeProjectState(State newState) public isAuthor {
+        require(project.state != newState, "New state must be different");
+        project.state = newState;
+        //Invoke the event kind to provide the relevant info
+        emit ProjectStateChanged(project.id, newState);
     }
 }
